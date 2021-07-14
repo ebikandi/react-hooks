150af6873d41a64c82e1f0b7c677a8f7eb8b31c6
ebikandi <ebikandi@gmail.com>
2021-07-14 08:10:59 +0200

    exercise 4

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,10 +2,10 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
+import {useLocalStorageState} from '../utils'
 
-function Board() {
+function Board({onClick, squares = []}) {
   // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
 
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
@@ -16,31 +16,10 @@ function Board() {
 
   // This is the function your square click handler will call. `square` should
   // be an index. So if they click the center square, this will be `4`.
-  function selectSquare(square) {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
-  }
-
-  function restart() {
-    // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
-  }
 
   function renderSquare(i) {
     return (
-      <button className="square" onClick={() => selectSquare(i)}>
+      <button className="square" onClick={() => onClick(i)}>
         {squares[i]}
       </button>
     )
@@ -48,8 +27,6 @@ function Board() {
 
   return (
     <div>
-      {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -65,25 +42,73 @@ function Board() {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button className="restart" onClick={restart}>
-        restart
-      </button>
     </div>
   )
 }
 
 function Game() {
+  // const STATE_KEY = 'tictactoe'
+  const [moves, setMoves] = React.useState([Array(9).fill(null)])
+  const [index, setIndex] = React.useState(0)
+
+  React.useEffect(() => {
+    setIndex(moves.length - 1)
+  }, [setIndex, moves.length])
+
+  const currentSquares = moves[index]
+
+  const nextValue = calculateNextValue(currentSquares)
+  const winner = calculateWinner(currentSquares)
+  const status = calculateStatus(winner, currentSquares, nextValue)
+
+  function restart() {
+    setMoves([Array(9).fill(null)])
+  }
+
+  function selectSquare(currentIndex) {
+    if (winner || currentSquares[currentIndex]) {
+      return
+    }
+
+    const squaresCopy = [...currentSquares]
+
+    squaresCopy[currentIndex] = nextValue
+
+    setMoves(prevMoves => {
+      return [...prevMoves, squaresCopy]
+    })
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board onClick={selectSquare} squares={currentSquares} />
+        <button className="restart" onClick={restart}>
+          restart
+        </button>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>
+          {moves.map((move, currentIndex) => (
+            <div>
+              <button
+                key={JSON.stringify(move)}
+                onClick={() => setIndex(currentIndex)}
+                disabled={index === currentIndex}
+              >
+                Go to move #{currentIndex}
+              </button>
+            </div>
+          ))}
+        </ol>
       </div>
     </div>
   )
 }
 
 // eslint-disable-next-line no-unused-vars
-function calculateStatus(winner, squares, nextValue) {
+function calculateStatus(winner, squares = [], nextValue) {
   return winner
     ? `Winner: ${winner}`
     : squares.every(Boolean)
@@ -92,12 +117,12 @@ function calculateStatus(winner, squares, nextValue) {
 }
 
 // eslint-disable-next-line no-unused-vars
-function calculateNextValue(squares) {
+function calculateNextValue(squares = []) {
   return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O'
 }
 
 // eslint-disable-next-line no-unused-vars
-function calculateWinner(squares) {
+function calculateWinner(squares = []) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
